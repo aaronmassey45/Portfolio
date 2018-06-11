28703e57963df00d1e8f4409c8daeb71b894df39
Aaron <aaronmassey45@gmail.com>
2018-06-10 22:20:24 -0500

    Created Header component and got it to display

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="card-panel amber mt-0">This is the Header component</div>
+    );
+  }
+}
