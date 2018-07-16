53a31792462e6f284df6d1c5a146dde27e4478bb
Aaron <aaronmassey45@gmail.com>
2018-07-16 11:28:36 -0500

    Moved sidenav initialization to Navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,27 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import M from 'materialize-css/dist/js/materialize.min';
 
 import NavButtons from './NavButtons';
 import logo from '../../assets/logo.svg';
 
-export default () => {
-  return (
-    <Fragment>
-      <nav className="white">
-        <div className="nav-wrapper container">
-          <a className="brand-logo logo">
-            <img src={logo} className="responsive-img" alt="logo" />
-          </a>
-          <a data-target="mobile" className="sidenav-trigger">
-            <i className="material-icons">menu</i>
-          </a>
-          <ul className="hide-on-med-and-down right">
-            <NavButtons />
-          </ul>
-        </div>
-      </nav>
-      <ul className="sidenav black" id="mobile">
-        <NavButtons sideNav={true} />
-      </ul>
-    </Fragment>
-  );
-};
+export default class extends Component {
+  componentDidMount() {
+    const elem = document.querySelector('.sidenav');
+    M.Sidenav.init(elem, {
+      edge: 'left',
+      inDuration: 250,
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <nav className="white">
+          <div className="nav-wrapper container">
+            <a className="brand-logo logo">
+              <img src={logo} className="responsive-img" alt="logo" />
+            </a>
+            <a data-target="mobile" className="sidenav-trigger">
+              <i className="material-icons">menu</i>
+            </a>
+            <ul className="hide-on-med-and-down right">
+              <NavButtons />
+            </ul>
+          </div>
+        </nav>
+        <ul className="sidenav black" id="mobile">
+          <NavButtons sideNav={true} />
+        </ul>
+      </Fragment>
+    );
+  }
+}
