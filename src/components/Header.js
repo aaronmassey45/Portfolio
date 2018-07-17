18f16eb8ec76548efb50ddd4bda600b9dfa5386a
Aaron <aaronmassey45@gmail.com>
2018-07-16 19:37:46 -0500

    Removed nav from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react';
-import M from 'materialize-css/dist/js/materialize.min';
-
-import NavButtons from './NavButtons';
 
 const style = {
   menuBtn: { position: 'fixed', top: 0, right: '16px' },
@@ -12,49 +9,22 @@ const style = {
 };
 
 export default class Header extends Component {
-  componentDidMount() {
-    const elem = document.querySelector('.sidenav');
-    M.Sidenav.init(elem, {
-      edge: 'left',
-      inDuration: 250,
-    });
-  }
-
   render() {
     return (
-      <header className="card-panel blue-grey mt-0 z-depth-3">
-        <div className="container center">
+      <header className="card-panel black mt-0 z-depth-2 white-text">
+        <div className="container">
+          <h4>
+            Hello, my name is <span className="saints-gold">Aaron</span>. I'm a
+            self-taught <span className="saints-gold">Full Stack Engineer</span>{' '}
+            living in <span className="saints-gold">San Antonio, TX</span>.
+          </h4>
           <img
             src="https://i.imgur.com/rXD35vk.png"
             alt="Avatar"
             className="circle responsive-img center-block"
             style={style.img}
           />
-          <h4 style={style.whiteText}>Aaron Massey</h4>
-          <h5>
-            <span className="title amber" style={style.whiteText}>
-              Full-Stack Web Engineer
-            </span>
-          </h5>
         </div>
-        <nav className="blue-grey z-depth-0">
-          <div className="nav-wrapper">
-            <a
-              data-target="mobile"
-              className="sidenav-trigger"
-              style={style.menuBtn}
-            >
-              <i className="material-icons">menu</i>
-            </a>
-            <ul className="hide-on-med-and-down center">
-              <NavButtons />
-            </ul>
-          </div>
-        </nav>
-
-        <ul className="sidenav blue-grey" id="mobile">
-          <NavButtons sideNav={true} />
-        </ul>
       </header>
     );
   }
