8df64786ae228756c32b238b1d085c551fbdb749
Aaron <aaronmassey45@gmail.com>
2018-07-27 12:07:29 -0500

    Added footer

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
           </Switch>
+          <footer id="main-footer">Copyright &copy; 2018</footer>
         </Fragment>
       </BrowserRouter>
     );
