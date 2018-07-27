e6e7a7331792f4b33294b9f9cc7a3926a265f5ed
Aaron <aaronmassey45@gmail.com>
2018-07-27 11:34:59 -0500

    Added permanent Home route and about route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { About, Navbar } from './components';
 import Header from './components/Header/Header';
-import Main from './components/MainContent/Main';
-
-const Home = () => (
-  <Fragment>
-    <Header />
-    <Main />
-  </Fragment>
-);
+import Home from './components/Home/Home';
+import About from './components/About/About';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
+        <Fragment>
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Fragment>
       </BrowserRouter>
     );
   }
