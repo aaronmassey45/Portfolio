98d49b42cf8ca6d5935109d5063eeea4c09eea4e
Aaron <aaronmassey45@gmail.com>
2018-07-27 12:06:51 -0500

    Adds bg-img id to body on mount

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import UserIcons from './UserIcons';
 
-export default () => {
-  return (
-    <main id="home">
-      <h1 className="lg-heading">
-        Aaron <span className="text-secondary">Massey</span>
-      </h1>
-      <h2 className="sm-heading">
-        Web Developer, Programmer, Dog Whisperer, & Fitness Guru.
-      </h2>
-      <div className="icons">
-        <UserIcons />
-      </div>
-    </main>
-  );
-};
+export default class extends Component {
+  componentDidMount() {
+    document.body.id = 'bg-img';
+  }
+
+  render() {
+    return (
+      <main id="home">
+        <h1 className="lg-heading">
+          Aaron <span className="text-secondary">Massey</span>
+        </h1>
+        <h2 className="sm-heading">
+          Web Developer, Programmer, Dog Whisperer, & Fitness Guru.
+        </h2>
+        <div className="icons">
+          <UserIcons />
+        </div>
+      </main>
+    );
+  }
+}
