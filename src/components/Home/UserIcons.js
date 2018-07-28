75016ea7ca468d1a350097c65d17364cffc864f6
Aaron <aaronmassey45@gmail.com>
2018-07-28 14:31:02 -0500

    Import SOCIAL from exports and open link in new tab

--- src/components/Home/UserIcons.js
+++ src/components/Home/UserIcons.js
@@ -1,30 +1,13 @@
 import React from 'react';
 
-const SOCIAL = [
-  {
-    name: 'linkedin',
-    link: '#',
-  },
-  {
-    name: 'instagram',
-    link: '#',
-  },
-  {
-    name: 'medium',
-    link: '#',
-  },
-  {
-    name: 'github',
-    link: '#',
-  },
-];
+import { SOCIAL } from '../../exports';
 
 export default () => {
   return (
     <div>
       {SOCIAL.map(({ name, link }) => {
         return (
-          <a href={link} key={name}>
+          <a href={link} key={name} target="_blank">
             <i className={`fab fa-${name} fa-2x`} />
           </a>
         );
