674c884132da5a2130086d5c17b6550d30fbbf41
Aaron <aaronmassey45@gmail.com>
2018-07-27 19:36:15 -0500

    Added Projects section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About';
+import Projects from './components/Projects/Projects';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
+            <Route path="/projects" component={Projects} />
           </Switch>
           <footer id="main-footer">Copyright &copy; 2018</footer>
         </Fragment>
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+const PROJECTS = [
+  {
+    name: 'project1',
+    img: require('../../assets/project1.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project2',
+    img: require('../../assets/project2.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project3',
+    img: require('../../assets/project3.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project4',
+    img: require('../../assets/project4.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project5',
+    img: require('../../assets/project5.jpg'),
+    to: '/projects/#',
+  },
+];
+
+export default class extends Component {
+  componentDidMount() {
+    document.body.id = '';
+  }
+
+  renderProjects = () => {
+    return PROJECTS.map(({ name, img, to }) => (
+      <div className="items" key={name}>
+        <Link to={to}>
+          <img src={img} alt="Project" />
+        </Link>
+        <Link to={to} className="btn-light">
+          <i className="fas fa-eye" /> {name}
+        </Link>
+        <Link to={to} className="btn-dark">
+          <i className="fab fa-github" /> GitHub
+        </Link>
+      </div>
+    ));
+  };
+
+  render() {
+    return (
+      <main id="projects">
+        <h1 className="lg-heading">
+          My <span className="text-secondary">Projects</span>.
+        </h1>
+        <h2 className="sm-heading">Check out some of my projects...</h2>
+        <div className="projects">{this.renderProjects()}</div>
+      </main>
+    );
+  }
+}
