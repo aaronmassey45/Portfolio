54d5161ba7bb844f14c89a72419a6f63d67e9db5
Aaron <aaronmassey45@gmail.com>
2018-07-28 17:14:06 -0500

    Updated contact page and associating styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import UserIcons from './Home/UserIcons';
+
+const email = 'cliffordaaronmassey@gmail.com';
+
 export default () => {
   return (
     <main id="contact">
@@ -7,19 +11,20 @@ export default () => {
         Contact <span className="text-secondary">Me</span>.
       </h1>
       <h2 className="sm-heading">This is how you can reach me...</h2>
-      <div className="boxes">
-        <div>
-          <span className="text-secondary">Email</span>{' '}
-          cliffordaaronmassey@gmail.com
+      <div className="content">
+        <div className="msg">
+          Have questions? In San Antonio and want to grab a coffee? Or better
+          yet, some tequila shots? Feel free to send me a message and I'll get
+          back to you as soon as possible!
         </div>
-        <div>
-          <span className="text-secondary">Phone</span> 1234567890
-        </div>
-        <div>
-          <span className="text-secondary">Address</span> 123 Suwoop Dr., Cali,
-          California
+
+        <div className="box">
+          <a href={`mailto:${email}`}>
+            <i className="text-secondary fas fa-envelope" /> {email}
+          </a>
         </div>
       </div>
+      <UserIcons />
     </main>
   );
 };
