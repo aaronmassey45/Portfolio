46ba713128c534ff4b23ebb8685b4c96e63c4292
Aaron <aaronmassey45@gmail.com>
2018-07-27 19:47:46 -0500

    Added contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 import Projects from './components/Projects/Projects';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route exact path="/" component={Home} />
             <Route path="/about" component={About} />
             <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
           </Switch>
           <footer id="main-footer">Copyright &copy; 2018</footer>
         </Fragment>
