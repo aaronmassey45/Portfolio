ffb10002efb1c913dec69a49c191d025b536d151
Aaron <aaronmassey45@gmail.com>
2018-07-28 14:31:38 -0500

    Added social

---
+++ src/exports.js
@@ -0,0 +1,46 @@
+export const PROJECTS = [
+  {
+    name: 'project1',
+    img: require('./assets/project1.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project2',
+    img: require('./assets/project2.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project3',
+    img: require('./assets/project3.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project4',
+    img: require('./assets/project4.jpg'),
+    to: '/projects/#',
+  },
+  {
+    name: 'project5',
+    img: require('./assets/project5.jpg'),
+    to: '/projects/#',
+  },
+];
+
+export const SOCIAL = [
+  {
+    name: 'linkedin',
+    link: 'https://www.linkedin.com/in/aaronmassey45/',
+  },
+  {
+    name: 'instagram',
+    link: 'https://www.instagram.com/_codepapi/',
+  },
+  {
+    name: 'medium',
+    link: 'https://medium.com/@aaronmassey45',
+  },
+  {
+    name: 'github',
+    link: 'https://github.com/aaronmassey45',
+  },
+];
