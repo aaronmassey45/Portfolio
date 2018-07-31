13632db2c424c996ebb098cecc01501ab0a5005b
Aaron <aaronmassey45@gmail.com>
2018-07-31 12:05:12 -0500

    Import SKILLS from exports and dynamically add them to page

--- src/components/About.js
+++ src/components/About.js
@@ -1,59 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/portrait.jpg';
 
-export default () => {
-  return (
-    <main id="about">
-      <h1 className="lg-heading">
-        About <span className="text-secondary">Me</span>.
-      </h1>
-      <h2 className="sm-heading">Let me tell you a few things...</h2>
-      <div className="about-info">
-        <img src={logo} alt="Aaron Massey" className="bio-image" />
-        <div className="bio">
-          <h3 className="text-secondary">BIO</h3>
-          <p>
-            Why do I say Engineer instead of Developer? Because I worked hard to
-            get my Electrical Engineering degree and am going to use it in some
-            capacity! I've always had a passion to create and while getting my
-            EE degree, I realized that coding scratched that itch better! I'm
-            currently working on a 100 Algorithm challenge, read all about it on
-            my{' '}
-            <b>
-              <a href="https://medium.com/@aaronmassey45">blog</a>
-            </b>! Outside of developing I have a beautiful girlfriend, a
-            handsome son, and two good ole puppers. I'd love to get coffee and
-            get to know you, so don’t hesitate to get in touch.
-          </p>
+import { SKILLS } from '../exports';
+
+export default class extends Component {
+  renderSkills = () => {
+    return SKILLS.map((skill, i) => {
+      return (
+        <div className={`skill skill-${i + 1}`} key={skill.title}>
+          <h2>{skill.title}</h2>
+          {skill.languages && (
+            <div>
+              <div className="skill__subheader">Languages I Speak</div>
+              <div>{skill.languages.join(', ')}</div>
+            </div>
+          )}
+          <div>
+            {skill.languages && (
+              <div className="skill__subheader">Dev Tools</div>
+            )}
+            <ul>{skill.devTools.map(tool => <li key={tool}>{tool}</li>)}</ul>
+          </div>
         </div>
-        <div className="job job-1">
-          <h3>123 Web Shop</h3>
-          <h6>Full Stack Developer</h6>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            dignissimos assumenda pariatur! Esse quod minima enim eveniet sed
-            nisi vel?
-          </p>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <main id="about">
+        <h1 className="lg-heading">
+          About <span className="text-secondary">Me</span>.
+        </h1>
+        <h2 className="sm-heading">Let me tell you a few things...</h2>
+        <div className="about-info">
+          <img src={logo} alt="Aaron Massey" className="bio-image" />
+          <div className="bio">
+            <h3 className="text-secondary">BIO</h3>
+            <p>
+              Why do I say Engineer instead of Developer? Because I worked hard
+              to get my Electrical Engineering degree and am going to use it in
+              some capacity! While working on my EE degree I realized the coding
+              aspect better scratched my itch to create. I'm currently working
+              on a 100 Algorithm challenge, read all about it on my{' '}
+              <b>
+                <a href="https://medium.com/@aaronmassey45">blog</a>
+              </b>! Outside of developing I have a beautiful girlfriend, a
+              handsome son, and two good ole puppers.
+            </p>
+          </div>
+          {this.renderSkills()}
         </div>
-        <div className="job job-2">
-          <h3>Design Squad</h3>
-          <h6>Front End Developer</h6>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            dignissimos assumenda pariatur! Esse quod minima enim eveniet sed
-            nisi vel?
-          </p>
-        </div>
-        <div className="job job-3">
-          <h3>Yard Sale</h3>
-          <h6>Cashier</h6>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            dignissimos assumenda pariatur! Esse quod minima enim eveniet sed
-            nisi vel?
-          </p>
-        </div>
-      </div>
-    </main>
-  );
-};
+      </main>
+    );
+  }
+}
