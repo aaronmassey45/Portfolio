c8cef2942ccc1d8f3834bd5aefbc8df03863a399
Aaron <aaronmassey45@gmail.com>
2018-08-02 20:41:40 -0500

    Updated component to use Image component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazyload';
 
+import Image from './Image';
 import { PROJECTS } from '../../exports';
 
 export default class extends Component {
   renderProjects = () => {
-    return PROJECTS.map(({ name, img, to }) => (
+    return PROJECTS.map(({ name, img, preload, to }) => (
       <div className="items" key={name}>
         <Link to={to}>
-          <img src={img} alt="Project" />
+          <Image srcPreload={preload} srcLoaded={img} name={name} />
         </Link>
         <Link to={to} className="btn-light">
           <i className="fas fa-eye" /> {name}
