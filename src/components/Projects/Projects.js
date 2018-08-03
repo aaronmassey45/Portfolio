b65e80277c58b2b7016e2c35fd4af7589bceadb6
Aaron <aaronmassey45@gmail.com>
2018-08-02 20:45:32 -0500

    Removed unused LazyLoad import

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import LazyLoad from 'react-lazyload';
 
 import Image from './Image';
 import { PROJECTS } from '../../exports';
