1899a7c954897f19fef0d60e2637023391d9f533
Aaron <aaronmassey45@gmail.com>
2018-08-27 16:08:34 -0500

    Updated projects

--- src/exports.js
+++ src/exports.js
@@ -1,29 +1,39 @@
 export const PROJECTS = [
   {
-    name: 'project1',
-    img: require('./assets/project1.jpg'),
-    to: '/projects/#',
+    name: 'Speedstudy Tracker',
+    img: require('./assets/speedstudy.png'),
+    preload: require('./assets/speedstudy-xs.png'),
+    to: 'https://speedstudy-chingu.herokuapp.com/',
+    github: 'https://github.com/chingu-voyage4/Bears-Team-3',
   },
   {
-    name: 'project2',
-    img: require('./assets/project2.jpg'),
-    to: '/projects/#',
+    name: 'Chowster',
+    img: require('./assets/chowster.png'),
+    preload: require('./assets/chowster-xs.png'),
+    to: 'https://chowster.glitch.me/',
+    github: 'https://github.com/aaronmassey45/Chowster',
   },
   {
-    name: 'project3',
-    img: require('./assets/project3.jpg'),
-    to: '/projects/#',
+    name: 'Hex/RGB Conversion',
+    img: require('./assets/hexrgb.png'),
+    preload: require('./assets/hexrgb-xs.png'),
+    to: 'https://aaronmassey.pro/hexrgb/',
+    github: 'https://github.com/aaronmassey45/180Websites-Day-52',
   },
   {
-    name: 'project4',
-    img: require('./assets/project4.jpg'),
-    to: '/projects/#',
+    name: 'Fizz Buzz!',
+    img: require('./assets/fizzbuzz.png'),
+    preload: require('./assets/fizzbuzz-xs.png'),
+    to: 'https://aaronmassey.pro/fizzbuzz/',
+    github: 'https://github.com/aaronmassey45/180Websites-Day-69',
   },
-  {
-    name: 'project5',
-    img: require('./assets/project5.jpg'),
-    to: '/projects/#',
-  },
+  // {
+  //   name: 'Rick-tacular Mortypedia',
+  //   img: require('./assets/rickmorty.png'),
+  //   preload: require('./assets/rickmorty-xs.png'),
+  //   to: '/projects/#',
+  //   github: 'https://github.com/aaronmassey45',
+  // },
 ];
 
 export const SOCIAL = [
