d6a353adc22da20c96cc9bdd5935f0e22e89bee5
Aaron <aaronmassey45@gmail.com>
2018-08-27 16:20:11 -0500

    Added 404 component

---
+++ src/components/NoMatch.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <main>
+      <h2 className="lg-heading">
+        You don't gotta go{' '}
+        <Link to="/" className="text-secondary">
+          <u>Home</u>
+        </Link>{' '}
+        but you shouldn't stay here...
+      </h2>
+    </main>
+  );
+};
