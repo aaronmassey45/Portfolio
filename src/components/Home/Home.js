428385267b808e4516ad3740b9241791fc5dbe9a
Aaron <aaronmassey45@gmail.com>
2019-04-27 07:55:46 -0500

    Refactored to functional component and added hooks

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 
 import UserIcons from './UserIcons';
 
-export default class extends Component {
-  componentDidMount() {
+const Home = () => {
+  useEffect(() => {
     document.body.id = 'bg-img';
-  }
 
-  componentWillUnmount() {
-    document.body.id = '';
-  }
+    return () => {
+      document.body.id = '';
+    };
+  }, []);
 
-  render() {
-    return (
-      <main id="home">
-        <h1 className="lg-heading">
-          Aaron <span className="text-secondary">Massey</span>
-        </h1>
-        <h2 className="sm-heading">
-          Full Stack Engineer, Dog Whisperer, & Fitness Guru.
-        </h2>
-        <UserIcons />
-      </main>
-    );
-  }
-}
+  return (
+    <main id="home">
+      <h1 className="lg-heading">
+        Aaron <span className="text-secondary">Massey</span>
+      </h1>
+      <h2 className="sm-heading">
+        Full Stack Engineer, Dog Whisperer, & Fitness Guru.
+      </h2>
+      <UserIcons />
+    </main>
+  );
+};
+
+export default Home;
