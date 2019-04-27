f23774044b17678c1b12e19235f62d23e9249b33
Aaron <aaronmassey45@gmail.com>
2019-04-27 07:27:08 -0500

    Refactored into functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
@@ -8,24 +8,20 @@ import Contact from './components/Contact';
 import Projects from './components/Projects/Projects';
 import NoMatch from './components/NoMatch';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Fragment>
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/contact" component={Contact} />
-            <Route component={NoMatch} />
-          </Switch>
-          <footer id="main-footer">Aaron Massey &copy; 2018</footer>
-        </Fragment>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/contact" component={Contact} />
+        <Route component={NoMatch} />
+      </Switch>
+      <footer id="main-footer">Aaron Massey &copy; 2018</footer>
+    </>
+  </Router>
+);
 
 export default App;
