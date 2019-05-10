aba6a567d749f2f0dda82534215c7ae4146b4406
Aaron <aaronmassey45@gmail.com>
2019-05-10 09:31:41 -0500

    Change import name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Projects from './components/Projects/Projects';
+import ProjectsList from './components/Projects/ProjectsList';
 import NoMatch from './components/NoMatch';
 
 const App = () => (
@@ -15,7 +15,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
+        <Route path="/projects" component={ProjectsList} />
         <Route path="/contact" component={Contact} />
         <Route component={NoMatch} />
       </Switch>
