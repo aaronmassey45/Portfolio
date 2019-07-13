775578314aab49eaff210c42c1bdc718f490f93d
Aaron <aaronmassey45@gmail.com>
2019-07-13 12:28:03 -0500

    Moved file into pages folder

--- src/pages/projects-page/ProjectsPage.js
+++ src/pages/projects-page/ProjectsPage.js
@@ -1,20 +1,21 @@
 import React from 'react';
 
-import Project from './Project';
-import { PROJECTS } from '../../exports';
+import Project from '../../components/Project';
 
-const ProjectList = () => (
+import PROJECTS_DATA from './projects-data';
+
+const ProjectsPage = () => (
   <main id="projects">
     <h1 className="lg-heading">
       My <span className="text-secondary">Projects</span>
     </h1>
     <h2 className="sm-heading">Check out some of my projects...</h2>
     <div className="projects">
-      {PROJECTS.map(props => (
+      {PROJECTS_DATA.map(props => (
         <Project {...props} key={props.name} />
       ))}
     </div>
   </main>
 );
 
-export default ProjectList;
+export default ProjectsPage;
