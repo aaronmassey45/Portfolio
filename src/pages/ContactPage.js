04131dc17de2c6c95bc3356f55dd0e4cda2da7db
Aaron <aaronmassey45@gmail.com>
2019-07-13 12:40:26 -0500

    Updated some page content

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -7,14 +7,13 @@ const email = 'cliffordaaronmassey@gmail.com';
 export default () => (
   <main id="contact">
     <h1 className="lg-heading">
-      Contact <span className="text-secondary">Me</span>.
+      Contact <span className="text-secondary">Me</span>
     </h1>
-    <h2 className="sm-heading">This is how you can reach me...</h2>
+    <h2 className="sm-heading">I'll get back to you as soon as possible</h2>
     <div className="content">
       <div className="msg">
         Have questions? In San Antonio and want to grab a coffee? Or better yet,
-        some tequila shots? Feel free to send me a message and I'll get back to
-        you as soon as possible!
+        some tequila shots? Feel free to send me a message!
       </div>
 
       <div className="box">
