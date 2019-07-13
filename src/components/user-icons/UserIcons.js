f07eb4dda5bc63a4126224e7568ee3a94d7c215c
Aaron <aaronmassey45@gmail.com>
2019-07-13 13:08:51 -0500

    Moved UserIcons into new folder

--- src/components/user-icons/UserIcons.js
+++ src/components/user-icons/UserIcons.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { SOCIAL } from '../exports';
+import SOCIAL_MEDIA_DATA from './social-data';
 
 export default () => {
   return (
     <div className="icons">
-      {SOCIAL.map(({ name, link }) => {
+      {SOCIAL_MEDIA_DATA.map(({ name, link }) => {
         return (
           <a href={link} key={name} target="_blank" rel="noopener noreferrer">
             <i className={`fab fa-${name} fa-2x`} />
