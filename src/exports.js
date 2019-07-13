56895f12d60ceefd9595a9e00150745c3351919b
Aaron <aaronmassey45@gmail.com>
2019-07-13 12:05:08 -0500

    Extracted skills data into it's own file

--- src/exports.js
+++ src/exports.js
@@ -82,34 +82,3 @@ export const SOCIAL = [
     link: 'https://github.com/aaronmassey45',
   },
 ];
-
-export const SKILLS = [
-  {
-    title: 'Front End Development',
-    languages: ['HTML', 'CSS', 'Javascript'],
-    devTools: [
-      'Bootstrap',
-      'GraphQL - Apollo',
-      'Material Design',
-      'React',
-      'Redux',
-      'Sass',
-    ],
-  },
-  {
-    title: 'Back End Development',
-    languages: ['Javascript', 'MongoDB'],
-    devTools: ['Express', 'GraphQL', "JWT's", 'Mongoose', 'NodeJs', 'oAuth'],
-  },
-  {
-    title: 'Other Tools',
-    devTools: [
-      'Git',
-      'GitHub',
-      'Deployment',
-      'NPM & Yarn',
-      'Responsive Design',
-      'ES6, ES7, ES8',
-    ],
-  },
-];
---
+++ src/pages/about-page/skills-data.js
@@ -0,0 +1,32 @@
+const SKILLS_DATA = [
+  {
+    title: 'Front End Development',
+    languages: ['HTML', 'CSS', 'Javascript'],
+    devTools: [
+      'Bootstrap',
+      'GraphQL - Apollo',
+      'Material Design',
+      'React',
+      'Redux',
+      'Sass',
+    ],
+  },
+  {
+    title: 'Back End Development',
+    languages: ['Javascript', 'MongoDB'],
+    devTools: ['Express', 'GraphQL', "JWT's", 'Mongoose', 'NodeJs', 'oAuth'],
+  },
+  {
+    title: 'Other Tools',
+    devTools: [
+      'Git',
+      'GitHub',
+      'Deployment',
+      'NPM & Yarn',
+      'Responsive Design',
+      'ES6, ES7, ES8',
+    ],
+  },
+];
+
+export default SKILLS_DATA;
