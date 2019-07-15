edfbfcb09d92e41063748ffe1837dec7660a69e8
Aaron <aaronmassey45@gmail.com>
2019-07-15 13:02:57 -0500

    Added button to view resume

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -3,19 +3,30 @@ import React from 'react';
 import UserIcons from '../components/user-icons/UserIcons';
 import ContactForm from '../components/contact-form/ContactForm';
 
-export default () => (
+import resume from '../assets/aaronMasseyResume.pdf';
+
+const ContactPage = () => (
   <main id="contact">
     <h1 className="lg-heading">
       Contact <span className="text-secondary">Me</span>
     </h1>
     <h2 className="sm-heading">Hit me up!</h2>
     <div className="content">
-      <div className="msg">
-        Have questions? In San Antonio and want to grab a coffee? Or better yet,
-        some tequila shots? Feel free to send me a message!
+      <div>
+        <div className="msg">
+          Have questions? In San Antonio and want to grab a coffee? Or better
+          yet, some tequila shots? Feel free to send me a message!
+        </div>
+        <div className="box">
+          <a href={resume} target="_blank">
+            View my resume
+          </a>
+        </div>
       </div>
       <ContactForm />
     </div>
     <UserIcons />
   </main>
 );
+
+export default ContactPage;
