b559b21a6f06387e9f9cf639d5581aaff994db41
Aaron <aaronmassey45@gmail.com>
2019-07-15 13:13:29 -0500

    Added link to portfolio github repo

--- src/pages/projects-page/ProjectsPage.js
+++ src/pages/projects-page/ProjectsPage.js
@@ -10,6 +10,17 @@ const ProjectsPage = () => (
       My <span className="text-secondary">Projects</span>
     </h1>
     <h2 className="sm-heading">Check out some of my projects...</h2>
+    <h4>
+      Click{' '}
+      <a
+        href="https://github.com/aaronmassey45/Portfolio"
+        className="text-secondary"
+        target="_blank"
+      >
+        here
+      </a>{' '}
+      to view the code for my portfolio.
+    </h4>
     <div className="projects">
       {PROJECTS_DATA.map(props => (
         <Project {...props} key={props.name} />
