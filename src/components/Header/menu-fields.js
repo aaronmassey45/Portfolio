4e3e67d3ad2eaf2897fa5906b6a6e21a7c43f6cc
Aaron <aaronmassey45@gmail.com>
2019-07-16 19:18:41 -0500

    Moved field object to separate file

---
+++ src/components/Header/menu-fields.js
@@ -0,0 +1,20 @@
+const FIELDS = [
+  {
+    to: '/',
+    title: 'Home',
+  },
+  {
+    to: '/about',
+    title: 'About Me',
+  },
+  {
+    to: '/projects',
+    title: 'Projects',
+  },
+  {
+    to: '/contact',
+    title: 'Contact',
+  },
+];
+
+export default FIELDS;
