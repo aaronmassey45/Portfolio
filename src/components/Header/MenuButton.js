e5f7c949803c505e2e4eb1c9bfbf49d21dd17a72
Aaron <aaronmassey45@gmail.com>
2019-07-16 19:20:34 -0500

    Stopped using context api and just use passed down props

--- src/components/Header/MenuButton.js
+++ src/components/Header/MenuButton.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
-import { HeaderContext } from './Header';
-
-const MenuButton = () => (
-  <HeaderContext.Consumer>
-    {context => (
-      <div
-        className={context.menuBtnClassList.join(' ')}
-        onClick={context.toggleMenu}
-      >
-        <div className="btn-line" />
-        <div className="btn-line" />
-        <div className="btn-line" />
-      </div>
-    )}
-  </HeaderContext.Consumer>
+const MenuButton = ({ isMenuShown, toggleMenu }) => (
+  <div
+    className={`menu-btn ${isMenuShown ? 'close' : ''}`}
+    onClick={toggleMenu}
+  >
+    <div className="btn-line" />
+    <div className="btn-line" />
+    <div className="btn-line" />
+  </div>
 );
 
 export default MenuButton;
