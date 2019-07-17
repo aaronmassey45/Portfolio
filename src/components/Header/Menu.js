b525f924f90e2d0943919a035ad214bf7504a8a6
Aaron <aaronmassey45@gmail.com>
2019-07-16 19:20:27 -0500

    Stopped using context api and just use passed down props

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -1,52 +1,28 @@
 import React from 'react';
-import { withRouter, Link } from 'react-router-dom';
 
-import { HeaderContext } from './Header';
+import NavItem from './NavItem';
+import FIELDS from './menu-fields';
 
-const FIELDS = [
-  {
-    to: '/',
-    title: 'Home',
-  },
-  {
-    to: '/about',
-    title: 'About Me',
-  },
-  {
-    to: '/projects',
-    title: 'Projects',
-  },
-  {
-    to: '/contact',
-    title: 'Contact',
-  },
-];
+const Menu = ({ isMenuShown, toggleMenu }) => {
+  const showClass = isMenuShown ? 'show' : '';
 
-const Menu = props => (
-  <HeaderContext.Consumer>
-    {context => (
-      <nav className={context.menuClassList.join(' ')}>
-        <div className={context.menuBrandingClassList.join(' ')}>
-          <div className="portrait" />
-        </div>
-        <ul className={context.menuNavClassList.join(' ')}>
-          {FIELDS.map(({ to, title }) => {
-            const classList = [...context.navItemClassList];
-            if (props.location.pathname === to && 'current') {
-              classList.push('current');
-            }
-            return (
-              <li className={classList.join(' ')} key={title}>
-                <Link to={to} className="nav-link" onClick={context.toggleMenu}>
-                  {title}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    )}
-  </HeaderContext.Consumer>
-);
+  return (
+    <nav className={`menu ${showClass}`}>
+      <div className={`menu-branding ${showClass}`}>
+        <div className="portrait" />
+      </div>
+      <ul className={`menu-nav ${showClass}`}>
+        {FIELDS.map(props => (
+          <NavItem
+            isShown={isMenuShown}
+            toggleMenu={toggleMenu}
+            {...props}
+            key={props.title}
+          />
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default withRouter(Menu);
+export default Menu;
