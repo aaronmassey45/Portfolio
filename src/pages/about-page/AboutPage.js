9a88e61d933d5c22e2645329eb64e4b381bce69e
Aaron <aaronmassey45@gmail.com>
2019-07-16 19:18:25 -0500

    Updated content

--- src/pages/about-page/AboutPage.js
+++ src/pages/about-page/AboutPage.js
@@ -17,13 +17,15 @@ const AboutPage = () => (
         <h3 className="text-secondary">BIO</h3>
         <p>
           My current focus is in Full Stack Development using the MERN stack
-          while expanding my knowledge in testing and dev ops. Why do I call my
-          self an Full Stack Engineer instead of Developer? I worked hard to get
-          my Bachelor's in Electrical Engineering and am going to use it in some
-          capacity. While working on my EE degree I realized I enjoyed my
-          courses where I got to code much more. After graduating I decided to
-          change career fields. I'm currently working on personal projects and
-          completing 100 algorithms, read all about it on my{' '}
+          while working on open source projects and expanding my knowledge in
+          testing. Why do I call myself a Full Stack Engineer instead of
+          Developer? I worked hard to get my Bachelor's in Electrical
+          Engineering and am going to use it in some capacity. While in college,
+          my friend and I were asked by the owner of the apartment complex we
+          worked at to build an iOS and Android application for the property so
+          I started learning Java. It was then that I knew that after graduating
+          I was going to change career fields. I'm currently working on personal
+          projects and completing 100 algorithms, read all about it on my{' '}
           <b>
             <a
               href="https://medium.com/@aaronmassey45"
