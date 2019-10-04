8984fe13d2275fde41ec7e9c5e279e37b77393f1
Aaron <aaronmassey45@gmail.com>
2019-10-04 18:56:02 -0500

    Use prop types Utilize new FormInput component Use custom snackbar Use absolute imports

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-import FormInput from '../form-input/FormInput';
-
+import FormInput from 'components/form-input/FormInput';
 import './contact-form.styles.scss';
 
-const ContactForm = () => {
+const ContactForm = ({ setSnackbarMsg }) => {
   const [fields, setFields] = useState({ name: '', message: '', email: '' });
   const { name, message, email } = fields;
 
@@ -22,24 +22,20 @@ const ContactForm = () => {
       });
       const json = await response.json();
       setFields({ name: '', message: '', email: '' });
-      alert(json.submission_text);
+      setSnackbarMsg(json.submission_text);
     } catch (error) {
-      console.log(error);
-      alert('Form did not submit, please try again');
+      setSnackbarMsg('Form did not submit, please try again');
     }
-    return;
   };
 
   const handleChange = e => {
-    const { name, value } = e.target;
-
-    setFields({ ...fields, [name]: value });
+    const { name: field, value } = e.target;
+    setFields({ ...fields, [field]: value });
   };
 
   return (
     <form onSubmit={handleSubmit} className="contact-form">
       <FormInput
-        type="text"
         name="name"
         value={name}
         onChange={handleChange}
@@ -47,19 +43,19 @@ const ContactForm = () => {
         required
       />
       <FormInput
-        type="text"
         name="email"
         value={email}
         onChange={handleChange}
         label="Email"
         required
+        type="email"
       />
       <FormInput
         name="message"
         value={message}
         onChange={handleChange}
         label="Message"
-        isTextArea
+        Element="textarea"
         required
       />
       <button className="button" type="submit">
@@ -68,5 +64,7 @@ const ContactForm = () => {
     </form>
   );
 };
+
+ContactForm.propTypes = { setSnackbarMsg: PropTypes.func.isRequired };
 
 export default ContactForm;
