109999b79045c12c8f1f48ffa212058ccf85eee2
Aaron <aaronmassey45@gmail.com>
2019-10-05 07:43:17 -0500

    Added propTypes

--- src/components/Header/NavItem.js
+++ src/components/Header/NavItem.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const NavItem = ({ isShown, to, title, toggleMenu }) => {
-  return (
-    <li className={`nav-item ${isShown && 'show'}`}>
-      <NavLink
-        activeClassName="current"
-        to={to}
-        className="nav-link"
-        onClick={toggleMenu}
-        exact
-      >
-        {title}
-      </NavLink>
-    </li>
-  );
+const NavItem = ({ isShown, to, title, toggleMenu }) => (
+  <li className={`nav-item ${isShown && 'show'}`}>
+    <NavLink
+      activeClassName="current"
+      to={to}
+      className="nav-link"
+      onClick={toggleMenu}
+      exact
+    >
+      {title}
+    </NavLink>
+  </li>
+);
+
+NavItem.propTypes = {
+  isShown: PropTypes.bool.isRequired,
+  to: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  toggleMenu: PropTypes.func.isRequired,
 };
 
 export default NavItem;
