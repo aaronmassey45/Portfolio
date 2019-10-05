c8f926dfa9e8fe6bbe349384308db7df9f42dca5
Aaron <aaronmassey45@gmail.com>
2019-10-05 07:39:14 -0500

    Menu can be toggled when pressing enter key while focused

--- src/components/Header/MenuButton.js
+++ src/components/Header/MenuButton.js
@@ -1,14 +1,23 @@
 import React from 'react';
 
-const MenuButton = ({ isMenuShown, toggleMenu }) => (
-  <div
-    className={`menu-btn ${isMenuShown ? 'close' : ''}`}
-    onClick={toggleMenu}
-  >
-    <div className="btn-line" />
-    <div className="btn-line" />
-    <div className="btn-line" />
-  </div>
-);
+const MenuButton = ({ isMenuShown, toggleMenu }) => {
+  const onKeyPress = e => {
+    if (e.key === 'Enter') toggleMenu();
+  };
+
+  return (
+    <div
+      className={`menu-btn ${isMenuShown ? 'close' : ''}`}
+      onClick={toggleMenu}
+      onKeyPress={onKeyPress}
+      role="button"
+      tabIndex={0}
+    >
+      <div className="btn-line" />
+      <div className="btn-line" />
+      <div className="btn-line" />
+    </div>
+  );
+};
 
 export default MenuButton;
