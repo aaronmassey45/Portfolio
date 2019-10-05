87394a01bb2027290324982090da897522414d2d
Aaron <aaronmassey45@gmail.com>
2019-10-05 07:49:52 -0500

    Remove unneeded return statements

--- src/components/user-icons/UserIcons.js
+++ src/components/user-icons/UserIcons.js
@@ -2,16 +2,14 @@ import React from 'react';
 
 import SOCIAL_MEDIA_DATA from './social-data';
 
-export default () => {
-  return (
-    <div className="icons">
-      {SOCIAL_MEDIA_DATA.map(({ name, link }) => {
-        return (
-          <a href={link} key={name} target="_blank" rel="noopener noreferrer">
-            <i className={`fab fa-${name} fa-2x`} />
-          </a>
-        );
-      })}
-    </div>
-  );
-};
+const UserIcons = () => (
+  <div className="icons">
+    {SOCIAL_MEDIA_DATA.map(({ name, link }) => (
+      <a href={link} key={name} target="_blank" rel="noopener noreferrer">
+        <i className={`fab fa-${name} fa-2x`} />
+      </a>
+    ))}
+  </div>
+);
+
+export default UserIcons;
